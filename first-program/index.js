3da8f17481c883164a6d2a846a04c6c21fa51bab
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-12 17:48:56 +0600

    first commit

---
+++ first-program/index.js
@@ -0,0 +1,5 @@
+var addNumber = function (n1, n2) {
+    return console.log(n1 + n2);
+};
+addNumber(12, 13);
+addNumber(12, 1);
---
+++ first-program/index.ts
@@ -0,0 +1,5 @@
+const addNumber = (n1:number, n2:number) => {
+  return console.log(n1 + n2)
+};
+addNumber(12,13)
+addNumber(12,1)
